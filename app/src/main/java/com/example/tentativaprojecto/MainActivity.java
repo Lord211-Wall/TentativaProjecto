e7282e07c27bd2c48676a9118e650fa6cee840c6
Martim211 <1181754@isep.ipp.pt>
2021-11-30 19:06:40 +0000

    Change

--- app/src/main/java/com/example/tentativaprojecto/MainActivity.java
+++ app/src/main/java/com/example/tentativaprojecto/MainActivity.java
@@ -1,10 +1,11 @@
 package com.example.tentativaprojecto;
 
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+Button butto;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
